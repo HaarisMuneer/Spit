36c61505eb2b80d18b28768ef22b8837e4166344
HaarisMuneer <hom9@cornell.edu>
2016-11-26 19:06:51 -0500

    swift 3 update

---
+++ Spit/Song.swift
@@ -0,0 +1,13 @@
+//
+//  Song.swift
+//  Spit
+//
+//  Created by Haaris Muneer on 8/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class Song: NSObject {
+
+}
